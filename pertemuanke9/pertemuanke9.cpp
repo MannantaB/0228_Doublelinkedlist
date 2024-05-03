87bf798790aed52b92cc9be8e64ac1db457d9152
MannantaB <mannanta18@gmail.com>
2024-05-03 20:49:02 +0700

    create case 5

--- pertemuanke9/pertemuanke9.cpp
+++ pertemuanke9/pertemuanke9.cpp
@@ -224,6 +224,9 @@ int main()
             case '4':
                 revtraverse();
                 break;
+            case '5':
+                searchData();
+                break;
             }
         }
     }
