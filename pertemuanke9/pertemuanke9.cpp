6456a2330fbed8b1b78f8025b06ee1652c38df91
MannantaB <mannanta18@gmail.com>
2024-05-03 20:29:15 +0700

    create prosedur revtraverse

--- pertemuanke9/pertemuanke9.cpp
+++ pertemuanke9/pertemuanke9.cpp
@@ -153,6 +153,25 @@ void traverse()
     }
 }
 
+void revtraverse()
+{
+    if (Listempty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecord in descending order of roll number are: " < endl;
+        Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
